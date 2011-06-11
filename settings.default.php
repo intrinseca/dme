420c8a75cdc8e2a1410c6b093b0bbc57844b8893
Michael Allan <michael@intrinseca.me.uk>
2011-06-11 12:16:48 +0100

    * Move database settings to separate file that will not be committed

---
+++ settings.default.php
@@ -0,0 +1,17 @@
+<?php
+
+/** 
+	* settings.default.php
+	* Contains database settings. This file should be renamed to settings.php and edited.
+	*/
+
+//File should not be opened directly
+if(!defined('DME'))
+{
+	die('This file is not an entry point');
+}
+
+$dsn = '';
+$user = '';
+$password = '';
+?>
